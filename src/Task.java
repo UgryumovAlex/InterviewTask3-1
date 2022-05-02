d66b3b1f6bf6001eded5d87de1bdf065d7f6fb7e
UgryumovAlex <79602945+UgryumovAlex@users.noreply.github.com>
2022-05-02 15:29:16 +0300

    Домашнее задание 3.1, "пинг - понг".

--- src/Task.java
+++ src/Task.java
@@ -1,6 +1,44 @@
 public class Task {
 
+    static final Object monitor = new Object();
+    static volatile int current = 0;
+
     public static void main(String[] args) {
-        System.out.println("ping-pong");
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    synchronized (monitor) {
+                        while (current == 1) {
+                            monitor.wait();
+                        }
+                        Thread.sleep(500);
+                        System.out.println("ping");
+                        current = 1;
+                        monitor.notify();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    synchronized (monitor) {
+                        while (current == 0) {
+                            monitor.wait();
+                        }
+                        Thread.sleep(500);
+                        System.out.println("pong");
+                        current = 0;
+                        monitor.notify();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
